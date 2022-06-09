ef47937bd221bed5757374f02c58af6710ad4ef2
Hamza Ishfaq <hamzaishfaq980@gmail.com>
2022-06-09 05:15:02 -0400

    made changes to mobile view

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-// THIS IS THE CODE FOR THE SLIDES ANIMATION ON THE MAIN PAGE.
-
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
-}
 
 
 
-// THIS IS THE CODE FOR THE HAMBURGER MENU FOR SMALLER SCREENS AND MOBILE SCREENS.
-
+// THIS IS THE CODE TO MAKE THE RESPONSIVE HAMBURGER MENU WORK FOR SMALL SCREENS/MOBILE.
 
 
 const toggle = document.getElementsByClassName('toggle')[0]
